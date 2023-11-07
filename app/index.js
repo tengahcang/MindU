8048fcc4dd2efe6f6683251583402e3dfe2a269f
Alfira Santa Praja <alfirasanta20@gmail.com>
2023-11-07 21:15:23 +0700

    komponen kategori

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Categories, ProfilUser,NavbarTop,} from '../components';
+import { ProfilUser,NavbarTop,} from '../components';
 export default function App() {
   return (
     <View style={styles.container}>
--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 import ProfilUser from './ProfilUser';
 import NavbarTop from './NavbarTop';
-import Categories from './Categories';
+import Categories from './Categories' ;
 export{
     ProfilUser,NavbarTop,Categories
 };
