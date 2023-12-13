b813f372e210cad6de0ebd914bfc2437192b3ac1
tengahcang <cangg522@gmail.com>
2023-12-13 22:02:42 +0700

    asu tai kucing

--- app/(tabs)/add.js
+++ app/(tabs)/add.js
@@ -2,17 +2,17 @@ import { View, Text } from 'native-base'
 import React from 'react'
 import { PrimaryButton,NavbarTopNew, Task,Separator } from '../../components'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 const add = () => {
   return (
     <SafeAreaView style={{flex:1, backgroundColor:'#D5DEEF',}}>
       <NavbarTopNew/>
       <View flexDirection={'row'} position={'absolute'} bottom={5}>
-        <Link href="AddScreen/AddTask" style={{textAlign:'center',justifyContent:'center'}}>
+        {/* <Link href="AddScreen/AddTask" style={{textAlign:'center',justifyContent:'center'}}> */}
           <View w={200} p={2}>
-            <PrimaryButton title="Tambah Task" color="#2196F3"/>
+            <PrimaryButton title="Tambah Task" color="#2196F3" onPress={()=>router.push("AddScreen/AddTask")}/>
           </View>
-        </Link>
+        {/* </Link> */}
         <Link href="AddScreen/AddKategori" style={{textAlign:'center',justifyContent:'center'}}>
           <View w={200} p={2}>
             <PrimaryButton title="Tambah Kategori" color="#2196F3"/>
--- app/AddScreen/AddTask.js
+++ app/AddScreen/AddTask.js
@@ -1,4 +1,4 @@
-import { View, Button ,Input,Text,TextArea, ScrollView} from 'native-base'
+import { View, Button ,Input,Text,TextArea, ScrollView, FormControl} from 'native-base'
 import React ,{useState} from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Separator, PrimaryButton ,UploadMedia} from '../../components'
@@ -8,6 +8,12 @@ const add = () => {
   const [date, setDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
+  const [Tugas,setTugas] = useState("");
+  const [Catatan,setCatatan] = useState("");
+  const [Deadline,setDeadline] = useState(new Date().toLocaleString());
+  const simpan = () =>{
+    console.log(Tugas,Catatan,Deadline)
+  }
 
   const onChangeDate = (event, selectedDate) => {
     setShowDatePicker(false);
@@ -40,53 +46,55 @@ const add = () => {
       <SafeAreaView>
         <Stack.Screen options={{headerTitle:"Add Task"}}/>
         <ScrollView >
-          <View p={4}>
-            <Text fontSize={20}>Nama Tugas</Text>
-            <Input size="lg" placeholder="Isi Nama Tugas" />
-            <Text fontSize={20}>Tugas Catatan</Text>
-            <TextArea size="lg" placeholder="Isi Catatan Tugas" />
-            <Text fontSize={20}>Deadline Tugas</Text>
-          {/* Button to show DateTimePicker for Date */}
-          </View>
-          <View p={4}>
-          <Button onPress={showDatepicker}>
-            <Text>Pick a Date</Text>
-          </Button>
-          <Separator height={20}/>
-          {/* Button to show DateTimePicker for Time */}
-          <Button onPress={showTimepicker}>
-            <Text>Pick a Time</Text>
-          </Button>
+          <FormControl>
+            <View p={4}>
+              <Text fontSize={20}>Nama Tugas</Text>
+              <Input size="lg" placeholder="Isi Nama Tugas" onChangeText={(Tugas) => setTugas(Tugas)} />
+              <Text fontSize={20}>Tugas Catatan</Text>
+              <TextArea size="lg" placeholder="Isi Catatan Tugas" onChangeText={(Catatan) => setCatatan(Catatan)} />
+              <Text fontSize={20}>Deadline Tugas</Text>
+            {/* Button to show DateTimePicker for Date */}
+            </View>
+            <View p={4}>
+            <Button onPress={showDatepicker}>
+              <Text>Pick a Date</Text>
+            </Button>
+            <Separator height={20}/>
+            {/* Button to show DateTimePicker for Time */}
+            <Button onPress={showTimepicker}>
+              <Text>Pick a Time</Text>
+            </Button>
 
-          {/* Date Picker */}
-          {showDatePicker && (
-            <DateTimePicker
-              value={date}
-              mode={"date"}
-              is24Hour={true}
-              onChange={onChangeDate}
-            />
-          )}
+            {/* Date Picker */}
+            {showDatePicker && (
+              <DateTimePicker
+                value={date}
+                mode={"date"}
+                is24Hour={true}
+                onChange={onChangeDate}
+              />
+            )}
 
-          {/* Time Picker */}
-          {showTimePicker && (
-            <DateTimePicker
-              value={date}
-              mode={"time"}
-              is24Hour={true}
-              onChange={onChangeTime}
-            />
-          )}
-          <Separator height={20}/>
-          {/* Display selected date and time */}
-          <Input size="lg" placeholder="Isi Deadline Tugas" value={date.toLocaleString()} />
+            {/* Time Picker */}
+            {showTimePicker && (
+              <DateTimePicker
+                value={date}
+                mode={"time"}
+                is24Hour={true}
+                onChange={onChangeTime}
+              />
+            )}
+            <Separator height={20}/>
+            {/* Display selected date and time */}
+            <Input size="lg" placeholder="Isi Deadline Tugas" value={Deadline} onChangeDate={(newDeadline)=>setDeadline(newDeadline)} />
 
-          <UploadMedia/>
-          </View>
-          <View p={10}>
-            <PrimaryButton title="Simpan" color="#2196F3"/>
-          </View>
-
+            <UploadMedia/>
+            </View>
+            <View p={10}>
+              <PrimaryButton title="Simpan" color="#2196F3" onPress={()=>simpan()}/>
+            </View>
+          </FormControl>
+          
         </ScrollView>
       </SafeAreaView>
     </View>
